b5ec1ab323eb603779799294d3d0dc04f069946a
techySPHINX <shimon123universe@gmail.com>
2024-06-28 00:19:28 +0530

    Set: SignUp Route

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -3,6 +3,7 @@ import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { send } from "process";
 
 export async function POST(request: Request) {
   await dbConnect();
@@ -18,12 +19,75 @@ export async function POST(request: Request) {
       return Response.json(
         {
           success: false,
-          message: "Username is already taken"
+          message: "Username is already taken",
         },
-        
+
         { status: 400 }
       );
     }
+
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserByEmail) {
+      //if user is registered
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exist with this email.", //no require of hard-coded
+          },
+          { status: 400 }
+        );
+      } else {
+        const hasedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hasedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save()
+      }
+    } else {
+      const hashedpassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedpassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newUser.save();
+    }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message, //no require of hard-coded
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully. Please verify your email.", //no require of hard-coded
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error reistering User", error);
     return Response.json(
